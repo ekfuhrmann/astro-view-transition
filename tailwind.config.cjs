f88d98690072e250a060c2ca1013c8e8bcfaa033
Eric <ekfuhrmann@gmail.com>
2023-09-27 17:19:11 -0600

    Persistent video and view transition playing

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  safelist: [
+    'bg-red-400',
+    'text-red-900',
+    'bg-black',
+    'text-white',
+    'bg-blue-400',
+    'text-blue-900',
+    'bg-cyan-900',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
 }
